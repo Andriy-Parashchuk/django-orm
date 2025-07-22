49b8ee72fe39745b947e6b631f8dbf4e8dd16372
Andriy-Parashchuk <andreiparaschyk1401@gmail.com>
2025-07-22 18:47:29 +0300

    Create models

--- school/grade_system/models.py
+++ school/grade_system/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    in_class = models.ForeignKey(Class, on_delete=models.CASCADE)
